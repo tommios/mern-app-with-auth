2c3a742f0629bbccb86028e3386f53c6491b726e
tommios <tommios.net@gmail.com>
2020-06-15 19:40:44 +0300

    add components Login and Register

--- client/src/App.js
+++ client/src/App.js
@@ -4,13 +4,17 @@ import "./App.css";
 
 import Navbar from "./components/layout/Navbar";
 import Landing from "./components/layout/Landing";
+import Register from "./components/auth/Register";
+import Login from "./components/auth/Login";
 
 function App() {
   return (
     <BrowserRouter>
       <div className="App">
         <Navbar />
-        <Landing />
+        <Route exact path="/" component={Landing} />
+        <Route exact path="/register" component={Register} />
+        <Route exact path="/login" component={Login} />
       </div>
     </BrowserRouter>
   );
