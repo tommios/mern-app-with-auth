aea359f1e31f1e0f35dc4ae3aebf57661a2f96fe
tommios <tommios.net@gmail.com>
2020-06-18 22:09:56 +0300

    add store.js and empty actions, reducers

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,9 @@ import React, { Component } from "react";
 import { BrowserRouter, Route } from "react-router-dom";
 import "./App.css";
 
+import { Provider } from "react-redux";
+import store from "./store";
+
 import Navbar from "./components/layout/Navbar";
 import Landing from "./components/layout/Landing";
 import Register from "./components/auth/Register";
@@ -9,14 +12,16 @@ import Login from "./components/auth/Login";
 
 function App() {
   return (
-    <BrowserRouter>
-      <div className="App">
-        <Navbar />
-        <Route exact path="/" component={Landing} />
-        <Route exact path="/register" component={Register} />
-        <Route exact path="/login" component={Login} />
-      </div>
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <div className="App">
+          <Navbar />
+          <Route exact path="/" component={Landing} />
+          <Route exact path="/register" component={Register} />
+          <Route exact path="/login" component={Login} />
+        </div>
+      </BrowserRouter>
+    </Provider>
   );
 }
 
